14f897507f566b9926a6ccb4b52a5e36995cf712
Michael Cao <mlc105@duke.edu>
2026-01-16 00:47:13 -0500

    Add files via upload

---
+++ index reliability coefficient analysis.R
@@ -0,0 +1,18 @@
+#title: "index reliability coefficient analysis"
+#date: "2025-05-20"
+ 
+#R packages to be installed (if not already) and loaded
+library(psych)
+
+#calculating Cronbach's alpha for the poverty index 
+df <- read.csv(file.choose()) #select the .csv file "combined_poverty_index_2002-2018.csv"
+povindex <- df[, c("z1", "z2", "z3", "z4", "z5")]
+alpha_result <- alpha(povindex, discrete = FALSE)
+alpha_result$total
+
+#calculating Cronbach's alpha for the population heterogeneity index 
+df <- read.csv(file.choose()) #select the .csv file "urban_added_combined_population_heterogeneity_index_2002-2018.csv"
+pophetindex <- df[, c("z1", "z2", "z3")]
+alpha_result <- alpha(pophetindex, discrete = FALSE)
+alpha_result$total
+
---
+++ linear spline interpolation of urbanicity.R
@@ -0,0 +1,21 @@
+#title: "linear spline interpolation of urbanicity"
+#date: "2025-05-20"
+
+#R packages to be installed (if not already) and loaded
+library(zoo)
+library(tidyverse)
+
+#Interpolating urbanicity
+data <- read.csv(file.choose()) #select the .csv file "PRE spline interpolation urban wide.csv" found in the raw data folder for the population heterogeneity index 
+data_long <- data.frame(data)
+data_long <- data_long %>% gather(key = "Year", value = "Value", c(-X1), na.rm = FALSE)
+head(data_long)
+data_long_interpolated <- data_long %>%
+  group_by(X1) %>%
+  arrange(Year) %>%
+  mutate(interpolated_value = zoo::na.spline(Value, na.rm = FALSE)) %>%
+  ungroup() %>%
+  arrange(X1, Year)
+data_long_interpolated$Year <- as.numeric(sub("X", "", data_long_interpolated$Year))
+write.csv(data_long_interpolated, file = "linear_spline_interpolation_urbanicity.csv")
+
